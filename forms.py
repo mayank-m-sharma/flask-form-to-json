52ce2ae418fc630e4e7820c3c5948dfc78c6962b
Mayank Sharma <mayanksharmasav97@gmail.com>
2019-03-13 21:54:40 +0530

    ..

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-
-
-class sampleForm(FlaskForm):
-	title  = StringField('Title')
-	detail = TextAreaField('Body')
-	submit = SubmitField('Save')
